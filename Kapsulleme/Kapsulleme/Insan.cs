e316f17568c6f81fada3ad866ce407f24d395221
Ferhat Akar <ferhatakar.1987@gmail.com>
2020-05-09 15:46:16 +0300

    Kapsulleme Kod
    Kapsulleme Kod

---
+++ Kapsulleme/Kapsulleme/Insan.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kapsulleme
+{
+    public class Insan
+    {
+        private string _adi;
+        private int _yas;
+        private string _meslek;
+        public bool _status=true;
+        //Adi Kapsulü 
+        public string Adi
+        {
+            get { return _adi;}//_adi olan özel değişkeni geri getirir.
+            set { _adi = value; } //gönderdiğimiz değeri uygular
+
+        }
+
+        public int Yas
+        {
+            get { return _yas; }
+            set {
+                if (value < 0 || value>100)
+                {
+                    MessageBox.Show("Yaş Değerini 1 ile 100 arasında bir değer Giriniz." + Environment.NewLine + "Değer: " + value, "Yaş Değerini Yanlış Girdiniz.");
+                    _status = false;//Yanlış Girildi ise Ekrana Yazdırtma
+                }
+                else
+                {
+                    _yas = value;
+                }
+            }
+        }
+
+        public string Meslek
+        {
+            get { return _meslek;}
+            set { _meslek = value; }
+        }
+        
+    }
+}
---
+++ Kapsulleme/Kapsulleme/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kapsulleme
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_yazdir_Click(object sender, RoutedEventArgs e)
+        {
+            Insan insan = new Insan();
+            insan.Adi = txt_name.Text;
+            insan.Meslek = txt_meslek.Text;
+            insan.Yas = Convert.ToInt32(txt_yas.Text);
+            EkranaYaz(insan);
+        }
+
+        public void EkranaYaz(Insan insan)
+        {
+            if (insan._status)
+            {
+                MessageBox.Show("Adi :" + insan.Adi + "\n" + "Yaşi :" + insan.Yas.ToString() + "\n" + "Meslek :" + insan.Meslek);
+            }
+        }
+    }
+}
